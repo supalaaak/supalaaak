e6dce3ff8813e2b2d2f06e39254256807a0f8fd7
supalaaak <supalaaak@gmail.com>
2026-04-14 08:59:29 +0700

    add path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   output: 'export', // Required for static sites
+  basePath: '/supalaaak', // Only if not using a custom domain
+  assetPrefix: '/supalaaak',
   images: { unoptimized: true }, // GitHub Pages doesn't support Next.js Image Optimization
 };
 export default nextConfig;
